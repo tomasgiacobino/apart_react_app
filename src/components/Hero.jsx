ef3edfa4dc7a06f6f8a0b1dd60f0ad55bda4b180
Tomas Giacobino <tgiacobino@pgmtools.com.ar>
2024-02-21 18:15:26 -0300

    logo del hero en mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,7 +80,7 @@ const Hero = () => (
           </div>
 
           <div className='logoHero'>
-              <img className='md:h-[200px] h-[200px] md:w-[250px] w-[200px] ssxs:mt-10 mt-0 ssxs:mr-7 mr-0' src={logoHeroSolo} alt="logoTerraMaria" />
+              <img className='md:h-[200px] h-[100px] md:w-[250px] w-[100px] ssxs:mt-10 mt-0 ssxs:mr-7 mr-0' src={logoHeroSolo} alt="logoTerraMaria" />
           </div>
 
         </div>
