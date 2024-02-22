f352c1370f351536d0ccd164b4980a3039f84192
Tomas Giacobino <tgiacobino@pgmtools.com.ar>
2024-02-22 13:40:31 -0300

    info de todos los aparts

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,7 +13,6 @@ import {
   photo10C1,
   photo11C1,
   photo12C1,
-
 } from "../assets/index";
 
 import {
@@ -29,34 +28,38 @@ import {
   photo10C2,
   photo11C2,
   photo12C2,
-  photo13C2,} from "../assets/index";
+  photo13C2,
+} from "../assets/index";
 
-  import {
-    photo1C3,
-    photo2C3,
-    photo3C3,
-    photo4C3,
-    photo5C3,
-    photo6C3,
-    photo7C3,
-    photo8C3,
-    photo9C3,
-    photo10C3,
-    photo11C3,
-    photo12C3,
-    photo13C3,} from "../assets/index";
+import {
+  photo1C3,
+  photo2C3,
+  photo3C3,
+  photo4C3,
+  photo5C3,
+  photo6C3,
+  photo7C3,
+  photo8C3,
+  photo9C3,
+  photo10C3,
+  photo11C3,
+  photo12C3,
+  photo13C3,
+} from "../assets/index";
 
-    import {photo1C4,
-      photo2C4,
-      photo3C4,
-      photo4C4,
-      photo5C4,
-      photo6C4,
-      photo7C4,
-      photo8C4,
-      photo9C4,
-      photo10C4,
-      photo11C4,} from "../assets/index";
+import {
+  photo1C4,
+  photo2C4,
+  photo3C4,
+  photo4C4,
+  photo5C4,
+  photo6C4,
+  photo7C4,
+  photo8C4,
+  photo9C4,
+  photo10C4,
+  photo11C4,
+} from "../assets/index";
 
 export const navLinks = [
   {
@@ -84,12 +87,12 @@ export const apartamentos = [
       "con un dormitorio, un baño, una cama doble y un sofá cama. Además, cuenta con una zona de trabajo y ofrece servicios" +
       "como wifi, aire acondicionado, heladera, TV de 34 pulgadas y secador de pelo. Se permiten mascotas.",
     photos: {
-      p1 : photo1C1,
-      p2 : photo2C1,
+      p1: photo1C1,
+      p2: photo2C1,
     },
     linkAirbnb: "https://www.airbnb.com.ar/rooms/744938814968353170?_set_bev_on_new_domain=1681475889_ZTFkMzI0OWE3Mjdi&source_impression_id=p3_1708386216_5I1XBeU5ETcYYSDC",
     linkBooking: "https://www.booking.com/hotel/ar/el-balcon-de-la-plaza.en-gb.html?aid=311984&label=el-balcon-de-la-plaza-9qTs5WBCyJBQH3hqTrvZlwS541095063578%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-78173750766%3Alp1000126%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YboIMJYQAPicrzwdxpGM5o8&sid=4aacf73a14ef2d58f6129d73a40c1aae&dest_id=-1011490;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1708386824;srpvid=f25ca80236b90242;type=total;ucfs=1&#hotelTmpl",
-    linkWpp: "https://wa.me/3874069341?text=Hola,%20estoy%20interesado%20en%20el%20apart%201%20de%20Terra%20Maria%20Apartment",
+    linkWpp: "https://wa.me/3874069341?text=Hola,%20estoy%20interesado%20en%20el%20Apart%201%20de%20Terra%20Maria%20Apartment",
     titleModal: "Terra María Apart 1",
     photosModal: [
       { original: photo3C1, thumbnail: photo3C1 },
@@ -107,17 +110,17 @@ export const apartamentos = [
   {
     id: "2",
     desc: "Este apartamento está diseñado para alojar cómodamente " +
-    "a tres huéspedes, con un dormitorio que cuenta con dos camas individuales y un baño. Además, " +
-    "ofrece servicios como wifi, aire acondicionado, " +
-    "televisor y secador de pelo. Este apartamento también incluye estacionamiento pago en las instalaciones.",
+      "a tres huéspedes, con un dormitorio que cuenta con dos camas individuales y un baño. Además, " +
+      "ofrece servicios como wifi, aire acondicionado, " +
+      "televisor y secador de pelo. Este apartamento también incluye estacionamiento pago en las instalaciones.",
     photos: {
-      p1 : photo1C2,
-      p2 : photo2C2,
+      p1: photo1C2,
+      p2: photo2C2,
     },
-    linkAirbnb: "https://www.airbnb.com.ar/rooms/744938814968353170?_set_bev_on_new_domain=1681475889_ZTFkMzI0OWE3Mjdi&source_impression_id=p3_1708386216_5I1XBeU5ETcYYSDC",
+    linkAirbnb: "https://www.airbnb.com.ar/rooms/816062808879160555?_set_bev_on_new_domain=1681475889_ZTFkMzI0OWE3Mjdi&source_impression_id=p3_1708396182_TorkPmKdhBMHU57Z",
     linkBooking: "https://www.booking.com/hotel/ar/el-balcon-de-la-plaza.en-gb.html?aid=311984&label=el-balcon-de-la-plaza-9qTs5WBCyJBQH3hqTrvZlwS541095063578%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-78173750766%3Alp1000126%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YboIMJYQAPicrzwdxpGM5o8&sid=4aacf73a14ef2d58f6129d73a40c1aae&dest_id=-1011490;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1708386824;srpvid=f25ca80236b90242;type=total;ucfs=1&#hotelTmpl",
-    linkWpp: "https://wa.me/3874069341?text=Hola,%20estoyyy%20interesado%20en%20el%20apart%201%20de%20Terra%20Maria%20Apartment",
-    titleModal: "Terra María Apart 1",
+    linkWpp: "https://wa.me/3874069341?text=Hola,%20estoy%20interesado%20en%20el%20Apart%202%20de%20Terra%20Maria%20Apartment",
+    titleModal: "Terra María Apart 2",
     photosModal: [
       { original: photo3C2, thumbnail: photo3C2 },
       { original: photo4C2, thumbnail: photo4C2 },
@@ -129,7 +132,59 @@ export const apartamentos = [
       { original: photo10C2, thumbnail: photo10C2 },
       { original: photo11C2, thumbnail: photo11C2 },
       { original: photo12C2, thumbnail: photo12C2 },
-      { original: photo13C2, thumbnail: photo13C2},
+      { original: photo13C2, thumbnail: photo13C2 },
+    ],
+  },
+  {
+    id: "3",
+    desc: "Este apartamento está diseñado para alojar cómodamente a cuatro huéspedes, " +
+      "con un dormitorio que ofrece dos camas dobles, un baño y una zona de trabajo." +
+      "Además, cuenta con servicios como wifi, aire acondicionado, televisor y secador de pelo.",
+    photos: {
+      p1: photo1C3,
+      p2: photo2C3,
+    },
+    linkAirbnb: "https://www.airbnb.com.ar/rooms/745007841653857708?_set_bev_on_new_domain=1681475889_ZTFkMzI0OWE3Mjdi&source_impression_id=p3_1708396527_0GNFlDQB9%2B%2FqZKoy",
+    linkBooking: "https://www.booking.com/hotel/ar/el-balcon-de-la-plaza.en-gb.html?aid=311984&label=el-balcon-de-la-plaza-9qTs5WBCyJBQH3hqTrvZlwS541095063578%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-78173750766%3Alp1000126%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YboIMJYQAPicrzwdxpGM5o8&sid=4aacf73a14ef2d58f6129d73a40c1aae&dest_id=-1011490;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1708386824;srpvid=f25ca80236b90242;type=total;ucfs=1&#hotelTmpl",
+    linkWpp: "https://wa.me/3874069341?text=Hola,%20estoy%20interesado%20en%20el%20Apart%203%20de%20Terra%20Maria%20Apartment",
+    titleModal: "Terra María Apart 3",
+    photosModal: [
+      { original: photo3C3, thumbnail: photo3C3 },
+      { original: photo4C3, thumbnail: photo4C3 },
+      { original: photo5C3, thumbnail: photo5C3 },
+      { original: photo6C3, thumbnail: photo6C3 },
+      { original: photo7C3, thumbnail: photo7C3 },
+      { original: photo8C3, thumbnail: photo8C3 },
+      { original: photo9C3, thumbnail: photo9C3 },
+      { original: photo10C3, thumbnail: photo10C3 },
+      { original: photo11C3, thumbnail: photo11C3 },
+      { original: photo12C3, thumbnail: photo12C3 },
+      { original: photo13C3, thumbnail: photo13C3 },
+    ],
+  },
+  {
+    id: "4",
+    desc: "Este apartamento está diseñado para alojar cómodamente a dos huéspedes, " +
+      "con un dormitorio que ofrece una cama doble, un baño y una zona de trabajo." +
+      "Además, cuenta con servicios como wifi, aire acondicionado, televisor y heladera.",
+    photos: {
+      p1: photo1C4,
+      p2: photo2C4,
+    },
+    linkAirbnb: "https://www.airbnb.com.ar/rooms/819352501400878741?_set_bev_on_new_domain=1681475889_ZTFkMzI0OWE3Mjdi&source_impression_id=p3_1707437984_kGXVAxguzRRYcufT",
+    linkBooking: "https://www.booking.com/hotel/ar/el-balcon-de-la-plaza.en-gb.html?aid=311984&label=el-balcon-de-la-plaza-9qTs5WBCyJBQH3hqTrvZlwS541095063578%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-78173750766%3Alp1000126%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YboIMJYQAPicrzwdxpGM5o8&sid=4aacf73a14ef2d58f6129d73a40c1aae&dest_id=-1011490;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1708386824;srpvid=f25ca80236b90242;type=total;ucfs=1&#hotelTmpl",
+    linkWpp: "https://wa.me/3874069341?text=Hola,%20estoy%20interesado%20en%20el%20Apart%204%20de%20Terra%20Maria%20Apartment",
+    titleModal: "Terra María Apart 4",
+    photosModal: [
+      { original: photo3C4, thumbnail: photo3C4 },
+      { original: photo4C4, thumbnail: photo4C4 },
+      { original: photo5C4, thumbnail: photo5C4 },
+      { original: photo6C4, thumbnail: photo6C4 },
+      { original: photo7C4, thumbnail: photo7C4 },
+      { original: photo8C4, thumbnail: photo8C4 },
+      { original: photo9C4, thumbnail: photo9C4 },
+      { original: photo10C4, thumbnail: photo10C4 },
+      { original: photo11C4, thumbnail: photo11C4 },
     ],
   }
 ]
